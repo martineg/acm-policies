447cceeb1eaca4e898a026b49e9eca23a53476b6
Martin Eggen <meggen@redhat.com>
2022-02-22 22:15:23 +0100

    More verbose bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,30 @@
 REG0=${REG0:-"opentlc"}
 ENV0=${ENV0:-"production"}
 
+tput setaf 3
+echo "Bootstrapping channels for ${REG0} tracking branch ${ENV0}"
+tput sgr0
+echo
+
+tput setaf 2
+echo  "Creating namespaces and channels"
 oc apply -f channels/${REG0}/
+tput sgr0
+echo
+
+tput setaf 2
+echo "Creating placement rules and bindings (packages)"
 oc apply -k packages/subscriptions/${ENV0}
+tput sgr0
+echo
+
+tput setaf 2
+echo "Creating subscription for policy subscriptions"
 oc apply -f policies/sb-${ENV0}.yaml
+tput sgr0
+echo
+
+tput setaf 3
+echo "All done"
+tput sgr0
+echo
